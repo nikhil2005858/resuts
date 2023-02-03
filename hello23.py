688a21000ea26e905899dd334150a4951c2c531e
nikhil2005858 <123183868+nikhil2005858@users.noreply.github.com>
2023-02-03 15:45:41 +0530

    Update hello23.py

--- hello23.py
+++ hello23.py
@@ -1,6 +1,4 @@
 import streamlit  as st
-st.image("download.PNG")
-st.header(" AVATAR TRAILER CLICK THE LINK TO ENJOY")
+st.header(" WELCOME TO GOVERNAMENT POLITECHNIC CHAMARAJANAGAR")
 st.write("[CLICK HERE>]( http://localhost:63342/GFFG/results1.html?_ijt=5l57eecgjohuqtf1tudbgo9kdp&_ij_reload=RELOAD_ON_SAVE)")
-x="WELCOME TO GOVERNAMENT POLITECHNIC CHAMARAJANAGAR"
 st.container()
